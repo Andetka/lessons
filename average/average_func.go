2bd5472d992c4b6ab2159038bd1a21a87e7c4bd0
arine heruntseva <undetka95@gmail.com>
2024-12-03 22:47:07 +0300

    new

---
+++ average/average_func.go
@@ -0,0 +1,17 @@
+package average_func
+
+import (
+	"fmt"
+)
+
+func Average(numbers ...float64) float64 {
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum / float64(len(numbers))
+}
+func main() {
+	fmt.Println(Average(100, 50))
+	fmt.Println(Average(90.7, 89.7, 98.5, 92.3))
+}
--- check.go
+++ check.go
@@ -1,13 +1,1 @@
 package main
-
-import (
-	"fmt"
-)
-
-func mix(flag bool, num int, strings ...string) {
-	fmt.Println(num, flag, strings)
-}
-func main() {
-	mix(true, 1, "a", "b")
-	mix(false, 2, "a", "b", "c", "d")
-}
